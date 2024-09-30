cb5d72f15385715979a2c5e691af80cc21378585
Bhaskar Adki <adkibhaskar2002@gmail.com>
2024-09-30 17:51:18 +0530

    "First Day Code"

---
+++ src/main/java/com/ecom/ShoppingCartApplication.java
@@ -0,0 +1,10 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+
+public class ShoppingCartApplication{
+    
+    public static void main(String[] args){
+        SpringApplication.run(ShoppingCartApplication.class, args);
+    }
+}
